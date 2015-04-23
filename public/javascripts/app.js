9cfd1cce168c7892271f3067480254645bf0259e
Kelly Talley <kcutchen@gmail.com>
2015-04-23 21:50:17 +0000

    converted album view to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -297,6 +297,12 @@ blocJams = angular.module('BlocJams', ['ui.router']);
      controller: 'Collection.controller',
      templateUrl: '/templates/collection.html'
    });
+
+   $stateProvider.state('album', {
+     url: '/album',
+     templateUrl: '/templates/album.html',
+     controller: 'Album.controller'
+    });
  }]);
  
  // This is a cleaner way to call the controller than crowding it on the module definition.
@@ -325,6 +331,38 @@ blocJams = angular.module('BlocJams', ['ui.router']);
      $scope.albums.push(angular.copy(albumPicasso));
     }
  }]);
+
+blocJams.controller('Album.controller', ['$scope', function($scope) {
+  $scope.album = angular.copy(albumPicasso);
+  var hoveredSong = null;
+   var playingSong = null;
+ 
+   $scope.onHoverSong = function(song) {
+     hoveredSong = song;
+   };
+ 
+   $scope.offHoverSong = function(song) {
+     hoveredSong = null;
+   };
+
+   $scope.getSongState = function(song) {
+     if (song === playingSong) {
+       return 'playing';
+     }
+     else if (song === hoveredSong) {
+       return 'hovered';
+     }
+     return 'default';
+   };
+
+   $scope.playSong = function(song) {
+      playingSong = song;
+    };
+ 
+    $scope.pauseSong = function(song) {
+      playingSong = null;
+    };
+}]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
